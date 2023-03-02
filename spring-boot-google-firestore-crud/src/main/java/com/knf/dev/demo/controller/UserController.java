9b664f2177cfb980554de9aa5f42fcb9f45f68b1
Sibin Rasiya <54763130+knowledgefactory4u@users.noreply.github.com>
2023-03-02 16:02:56 +0530

    Update UserController.java

--- spring-boot-google-firestore-crud/src/main/java/com/knf/dev/demo/controller/UserController.java
+++ spring-boot-google-firestore-crud/src/main/java/com/knf/dev/demo/controller/UserController.java
@@ -10,7 +10,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1")
 public class UserController {
 
     @Autowired
