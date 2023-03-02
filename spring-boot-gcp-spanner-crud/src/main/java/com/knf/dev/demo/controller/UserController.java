e9be5690b83929c2e1e24cf5322f8f079d0d4d4e
Sibin Rasiya <54763130+knowledgefactory4u@users.noreply.github.com>
2023-03-02 16:03:59 +0530

    Update UserController.java

--- spring-boot-gcp-spanner-crud/src/main/java/com/knf/dev/demo/controller/UserController.java
+++ spring-boot-gcp-spanner-crud/src/main/java/com/knf/dev/demo/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
