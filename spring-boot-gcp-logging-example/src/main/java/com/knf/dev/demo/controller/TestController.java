7c4883292fb7eaaf5724a1acb0750d4c70d82416
Sibin Rasiya <54763130+knowledgefactory4u@users.noreply.github.com>
2023-03-13 23:16:37 +0530

    Add files via upload

---
+++ spring-boot-gcp-logging-example/src/main/java/com/knf/dev/demo/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.knf.dev.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/** Sample REST Controller to demonstrate Stackdriver Logging. */
+@RestController
+public class TestController {
+
+    private static final Log LOGGER = LogFactory.getLog(TestController.class);
+
+    @GetMapping("/test")
+    public String test() {
+
+        LOGGER.info("Info 1 - Inside test method");
+        LOGGER.info("Info 2 - Inside test method");
+
+        return "Testing.....";
+    }
+}
