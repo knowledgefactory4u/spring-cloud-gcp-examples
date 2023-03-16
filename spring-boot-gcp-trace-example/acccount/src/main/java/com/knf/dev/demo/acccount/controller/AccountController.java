1cbfb1adc85210d5da82e8853d8a890cba5f15ac
Sibin Rasiya <54763130+knowledgefactory4u@users.noreply.github.com>
2023-03-16 20:08:27 +0530

    Add files via upload

---
+++ spring-boot-gcp-trace-example/acccount/src/main/java/com/knf/dev/demo/acccount/controller/AccountController.java
@@ -0,0 +1,30 @@
+package com.knf.dev.demo.acccount.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+public class AccountController {
+
+    private static final Logger LOG = LoggerFactory.
+            getLogger(AccountController.class);
+        @Autowired
+        RestTemplate restTemplate;
+
+        @GetMapping(value = "/account")
+        public String account() {
+
+            LOG.info("Inside account service");
+            String responseFromCartService = restTemplate.
+                   getForObject("http://localhost:9090/cart",
+                    String.class);
+            LOG.info("Successfully communicated " +
+               "with cart service");
+
+            return responseFromCartService+" and Account Service running...";
+        }
+    }
---
+++ spring-boot-gcp-trace-example/cart/src/test/java/com/knf/dev/demo/cart/Application.java
@@ -0,0 +1,13 @@
+package com.knf.dev.demo.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
