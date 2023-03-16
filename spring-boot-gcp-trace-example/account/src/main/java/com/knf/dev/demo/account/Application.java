702e4c489e686db7fc3230f1b4c63f5482a737bd
Sibin Rasiya <54763130+knowledgefactory4u@users.noreply.github.com>
2023-03-16 20:33:22 +0530

    Add files via upload

---
+++ spring-boot-gcp-trace-example/account/src/main/java/com/knf/dev/demo/account/Application.java
@@ -0,0 +1,20 @@
+package com.knf.dev.demo.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
---
+++ spring-boot-gcp-trace-example/account/src/test/java/com/knf/dev/demo/account/Application.java
@@ -0,0 +1,13 @@
+package com.knf.dev.demo.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
