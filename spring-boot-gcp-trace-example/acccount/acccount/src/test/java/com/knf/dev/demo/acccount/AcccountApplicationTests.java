a42961461b6ff7cbb3c139cdd8f3fa9d3d8b47c6
Sibin Rasiya <54763130+knowledgefactory4u@users.noreply.github.com>
2023-03-16 19:57:45 +0530

    Add files via upload

---
+++ spring-boot-gcp-trace-example/acccount/acccount/src/test/java/com/knf/dev/demo/acccount/AcccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knf.dev.demo.acccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
